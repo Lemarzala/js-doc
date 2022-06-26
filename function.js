4800ca9ac7212b6ac6f5fa81185f47192c51e149
lemarzala <lemarzala.tarakhil123@gmail.com>
2022-06-25 23:19:34 -0600

    push

---
+++ function.js
@@ -0,0 +1,12 @@
+function grumpus() {
+  console.log("ugh...you again...");
+  console.log("FOR THE LAST TIME...");
+  console.log("LEAVE ME ALONE!!!");
+}
+grumpus();
+grumpus();
+grumpus();
+
+for (let i = 0; i < 50; i++) {
+  grumpus();
+}
