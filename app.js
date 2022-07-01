b6ccde791778ad3f074ad223b374a05f1f567116
lemarzala <lemarzala.tarakhil123@gmail.com>
2022-06-30 23:23:23 -0600

    push

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-let nums = [1, 2, 3];
-let mystery = [1, 2, 3];
-let moreNums = nums;
-
-nums === mystery;
-
-nums === moreNums;
-
-const user = {
-  username: "CherryGarcia8",
-  email: "garcia@gmail.com",
-  notifications: ["message", "alert"],
-};
-
-if (user.notifications === []) {
-  console.log("NO NEW NOTIFICATIONS!");
+// if statement
+// let rating = 4;
+// if (rating === 3) {
+//   console.log("you are a superstar!");
+// } else if (rating === 2) {
+//   console.log("byeee");
+// } else {
+//   console.log("wrong");
+// }
+//
+// &&
+// let password = "asra123";
+// if (password.length >= 6 && password.indexOf("") === -1) {
+//   console.log("valid password");
+// } else {
+//   console.log("invalid pass");
+// }
+||
+let age = 9;
+if (age < 6 || age >= 65) {
+  console.log("you get in for free");
+} else {
+  console.log("10$ please");
 }
 
-if (!user.notifications.length) {
-  console.log("NO NEW NOTIFICATIONS!");
-}
