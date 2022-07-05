6ad488e5f980369f4dfbbd88379a8356f5b639e4
lemarzala <lemarzala.tarakhil123@gmail.com>
2022-07-05 17:11:47 -0600

    push for loop

---
+++ loops.js
@@ -0,0 +1,14 @@
+for (let i = 1; i <= 10; i += 3) {
+  console.log("HELLO:", i);
+}
+
+// Printing first 20 perfect squares:
+for (let num = 1; num <= 20; num++) {
+  console.log(`${num}x${num} = ${num * num}`);
+}
+
+// Counting DOWN from 200 by intervals of 25:
+for (let i = 200; i >= 0; i -= 25) {
+  console.log(i);
+}
+console.log("AFTER THE LOOP!");
