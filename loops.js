ce0c91d93b527bbf0637b1ed7aac668bf38e369b
lemarzala <lemarzala.tarakhil123@gmail.com>
2022-07-05 22:17:52 -0600

    function challenge

--- loops.js
+++ loops.js
@@ -1,5 +1,10 @@
-const animals = ["lion", "tigers", "bear"];
-
-for (let i = 0; i < animals.length; i++) {
-  console.log(i, animals[i]);
+const target = Math.floor(Math.random() * 10);
+let guess;
+//ANOTHER APPROACH TO THE PREVIOUS GUESSING 'GAME'
+while (true) {
+  if (target === guess) break; //Break stops the loop in its tracks
+  console.log(`Target: ${target} Guess: ${guess}`);
+  guess = Math.floor(Math.random() * 10);
 }
+console.log(`Target: ${target} Guess: ${guess}`);
+console.log(`CONGRATS YOU WIN!!`);
