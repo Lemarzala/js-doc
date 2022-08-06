2772d92724dd0acc4cafee63384b7146f42d7c03
lemarzala <lemarzala.tarakhil123@gmail.com>
2022-08-06 00:19:13 -0600

    push

--- loops.js
+++ loops.js
@@ -1,10 +1,11 @@
-const target = Math.floor(Math.random() * 10);
-let guess;
-//ANOTHER APPROACH TO THE PREVIOUS GUESSING 'GAME'
-while (true) {
-  if (target === guess) break;
-  console.log(`Target: ${target} Guess: ${guess}`);
-  guess = Math.floor(Math.random() * 10);
-}
-console.log(`Target: ${target} Guess: ${guess}`);
-console.log(`CONGRATS YOU WIN!!`);
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
