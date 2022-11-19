cc95cf31211cf4a93f808bbd2253b02d4a457e5b
lemarzala <lemarzala.tarakhil123@gmail.com>
2022-11-18 23:36:07 -0700

    push

--- reduce.js
+++ reduce.js
@@ -1,9 +1,7 @@
-const votes = ['y', 'y', 'n', 'y', 'n', 'y', 'n', 'y', 'n', 'n', 'n', 'y', 'y'];
-
+const votes = ["y", "y", "n", "y", "n", "y", "n", "y", "n", "n", "n", "y", "y"];
 
 const results = votes.reduce((tally, val) => {
   tally[val] = (tally[val] || 0) + 1;
   return tally;
 }, {});
-...results
-
+results;
